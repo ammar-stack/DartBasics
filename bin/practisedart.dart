9eca66e03ff36deb611bbd883579fea5ccc07cdb
Ammar <mohammadammar872@gmail.com>
2023-11-20 04:33:17 -0800

    first commit

---
+++ bin/practisedart.dart
@@ -0,0 +1,34 @@
+import 'package:practisedart/practisedart.dart' as practisedart;
+
+void main(){
+  //DataTypes in Dart
+  //one - var(var can handle all data types of string,int,double,bool,char)
+  var name = 'Ammar';
+  var age = 23;
+  var percentage = 85.5;
+  var grade = 'A';
+  print('My name is $name');
+  print('My age is $age');
+  print('My percentage in matric is $percentage');
+  print('My grade in matric is $grade');
+
+  //two - string
+  String interest = 'My interest is playing football';
+  print(interest);
+
+  //three - int and double
+  int S_number = 30;
+  double weight = 85.5;
+  print(S_number);
+  print(weight);
+
+  //four - num
+  num games= 30;
+  num length = 5.5;
+  print(games);
+  print(length);
+
+  
+
+
+}
---
+++ test/practisedart_test.dart
@@ -0,0 +1,8 @@
+import 'package:practisedart/practisedart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
